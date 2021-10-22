045dd61cba9ce9931ef165e9221f7b24b44c8ff7
Sharkadi Andrey <sharkadi.a@gmail.com>
2021-10-22 17:20:52 +0300

    More text and fixes

---
+++ src/JsonEasyNavigation.Tests/ExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using Shouldly;
+using Xunit;
+
+namespace JsonEasyNavigation.Tests
+{
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void UsingPath_ShouldFindProperty()
+        {
+            var json = @"{ ""item1"" : { ""item2"" : { ""item3"" : ""test-string"" } } }";
+            
+            var jsonDocument = JsonDocument.Parse(json);
+            var nav = jsonDocument.RootElement.Property("item1", "item2", "item3");
+
+            nav.Exist.ShouldBeTrue();
+            nav.GetStringOrDefault().ShouldBe("test-string");
+        }        
+        
+        [Fact]
+        public void UsingPath_ShouldNotFindProperty()
+        {
+            var json = @"{ ""item1"" : { ""item2"" : { ""item3"" : ""test-string"" } } }";
+            
+            var jsonDocument = JsonDocument.Parse(json);
+            var nav = jsonDocument.RootElement.Property("item1", "item2", "item3", "item4");
+
+            nav.Exist.ShouldBeFalse();
+        }
+        
+        [Fact]
+        public void UsingPathAndGetProperty_ShouldFindProperty()
+        {
+            var json = @"{ ""item1"" : { ""item2"" : { ""item3"" : { ""item4"" : 100 } } } }";
+            
+            var jsonDocument = JsonDocument.Parse(json);
+            var nav = jsonDocument.RootElement.Property("item1", "item2", "item3")["item4"];
+
+            nav.Exist.ShouldBeTrue();
+            nav.GetInt32OrDefault().ShouldBe(100);
+        }
+    }
+}
--- src/JsonEasyNavigation/PropertyPathVisitor.cs
+++ src/JsonEasyNavigation/PropertyPathVisitor.cs
@@ -6,8 +6,14 @@
         {
             while (true)
             {
-                if (properties.Count < 1) return default;
+                if (properties.Count < 1)
+                {
+                    return new NavigationElement(jsonElement, false);
+                }
+                
                 var property = properties[0];
+                if (jsonElement.ValueKind != JsonValueKind.Object) return default;
+                
                 if (!jsonElement.TryGetProperty(property, out var result)) return default;
 
                 jsonElement = result;
